86c637c7e915c9759b3fc6f0da6b9bbea5ff42d7
mr8356 <mr8356@naver.com>
2025-04-29 14:26:04 +0900

    init

---
+++ server/src/main/java/com/cashticket/CashTicket.java
@@ -0,0 +1,11 @@
+package com.cashticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashTicket {
+	public static void main(String[] args) {
+		SpringApplication.run(CashTicket.class, args);
+	}
+}
