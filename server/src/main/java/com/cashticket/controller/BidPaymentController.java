1a0497a57e23f883141e0a8cff582982df8318ad
mr8356 <mr8356@naver.com>
2025-06-21 15:26:25 +0900

    toss

--- server/src/main/java/com/cashticket/controller/BidPaymentController.java
+++ server/src/main/java/com/cashticket/controller/BidPaymentController.java
@@ -1,24 +1,49 @@
 package com.cashticket.controller;
 
+import com.cashticket.config.CurrentUser;
 import com.cashticket.dto.BidOrderRequest;
 import com.cashticket.dto.BidOrderResponse;
 import com.cashticket.entity.User;
 import com.cashticket.service.BidOrderService;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 
 // controller/BidPaymentController.java
-@RestController
+@Controller
 @RequiredArgsConstructor
 @RequestMapping("/api/auction")
 public class BidPaymentController {
 
     private final BidOrderService bidOrderService;
 
-    @PostMapping("/orders")
-    public BidOrderResponse createOrder(@RequestBody BidOrderRequest req,
-                                        User user) {
-        return bidOrderService.createOrder(req, user.getId());
+    @GetMapping("/orders")
+    public String createOrder(@CurrentUser User user,
+     @RequestParam String concertId,
+      @RequestParam String amount,
+       Model model) {
+        model.addAttribute("concertId", concertId);
+        model.addAttribute("amount", amount);
+        model.addAttribute("user", user);
+        return "/payment/index";
+    }
+
+    @GetMapping("/success")
+    public String success(@CurrentUser User user, @RequestParam String concertId, @RequestParam String amount) {
+        bidOrderService.createOrder(Long.parseLong(concertId), Long.parseLong(amount), user.getId());
+        return "/payment/success";
+    }
+
+    @GetMapping("/fail")
+    public String fail(@CurrentUser User user) {
+        return "/payment/fail";
+    }
+
+    @GetMapping("/cancel")
+    public String cancel(@CurrentUser User user) {
+        return "/payment/cancel";
     }
 }
